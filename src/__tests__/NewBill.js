a0c788acc6069de12f05f22416ad65a5b9f2711d
Eri Schon <erischon@gmail.com>
2023-04-25 17:28:10 +0200

    Update NewBill.js

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -92,3 +92,83 @@ describe("Given I am connected as an employee", () => {
     });
   });
 });
+
+//
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to NewBill Page", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills");
+
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "a@a",
+        })
+      );
+
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+    });
+
+    test("fetches bills from mock API GET", async () => {
+      // Arrange
+      const store = await mockStore.bills().list();
+
+      // Act
+      window.onNavigate(ROUTES_PATH.Bills);
+      const resultType = screen.getByText(store[0].type);
+      const resultName = screen.getByText(store[0].name);
+
+      // Assert
+      expect(store[0].type).toBeTruthy();
+      expect(store[0].name).toBeTruthy();
+    });
+
+    describe("When an error occurs on API", () => {
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        // Arrange
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"));
+            },
+          };
+        });
+
+        // Act
+        window.onNavigate(ROUTES_PATH.Dashboard);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+
+        // Assert
+        expect(message).toBeTruthy();
+      });
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        // Arrange
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"));
+            },
+          };
+        });
+
+        // Act
+        window.onNavigate(ROUTES_PATH.Dashboard);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+
+        // Assert
+        expect(message).toBeTruthy();
+      });
+    });
+  });
+});
